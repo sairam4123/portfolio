69a4212b06e2f13eebf2be4fbcf46b742e466e72
Sairam M <sairam.m.2005.65@gmail.com>
2026-06-18 14:43:55 +0530

    feat: implement robots.txt functionality and update sitemap last modified date

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+// app/robots.ts
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://sairamthedev.vercel.app/sitemap.xml",
+  };
+}
